2241d6d5d0d8ede7a29d0acf559531b45fa2ffce
Haolun <chshawkn@hotmail.com>
2017-04-10 17:12:32 +0800

    init commit

---
+++ src/main/maven/maven_opts_github.sh
@@ -0,0 +1,23 @@
+
+
+#GIT_SERVICE_HOST=in .travis.yml
+
+# TODO publish to docker-hub
+export DOCKER_REGISTRY="dockerhub"
+# TODO github maven site
+export MAVEN_OPTS="${MAVEN_OPTS} -Dgithub-mvnsite.prefix="
+#export MAVEN_OPTS="${MAVEN_OPTS} -Dgithub-nexus.mirror=no mirror"
+export MAVEN_OPTS="${MAVEN_OPTS} -Dnexus.github.repositories=https://oss.sonatype.org/content/repositories"
+#export MAVEN_OPTS="${MAVEN_OPTS} -Dgithub-sonar.host.url=https://sonarqube.com"
+export MAVEN_OPTS="${MAVEN_OPTS} -Dcheckstyle.config.location=${BUILD_SCRIPT_LOC}/src/main/checkstyle/google_checks_6.19.xml"
+export MAVEN_OPTS="${MAVEN_OPTS} -Dpmd.ruleset.location=${BUILD_SCRIPT_LOC}/src/main/pmd/pmd-ruleset-5.3.5.xml"
+
+export MAVEN_OPTS="${MAVEN_OPTS} -Dinfrastructure=${INFRASTRUCTURE}"
+export MAVEN_OPTS="${MAVEN_OPTS} -Ddocker.registry=${DOCKER_REGISTRY}"
+export MAVEN_OPTS="${MAVEN_OPTS} -Dsite=${BUILD_SITE}"
+export MAVEN_OPTS="${MAVEN_OPTS} -Duser.language=zh -Duser.region=CN -Dfile.encoding=UTF-8 -Duser.timezone=Asia/Shanghai"
+export MAVEN_OPTS="${MAVEN_OPTS} -Dmaven.test.failure.ignore=${BUILD_TEST_FAILURE_IGNORE}"
+export MAVEN_OPTS="${MAVEN_OPTS} -Dfrontend.nodeDownloadRoot=https://npm.taobao.org/mirrors/node/"
+export MAVEN_OPTS="${MAVEN_OPTS} -Dfrontend.npmDownloadRoot=http://registry.npm.taobao.org/npm/-/"
+
+echo "MAVEN_OPTS: ${MAVEN_OPTS}"
